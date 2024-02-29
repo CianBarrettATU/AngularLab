883a982afdda7e43102cf6b3f357607ab20e2fdf
CianBarrettATU <G00416157@atu.ie>
2024-02-29 15:42:20 +0000

    lab4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListComponentComponent } from './list-component/list-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'data-app';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  counter:number = 0;
+  hide:boolean = true;
+  hideBtnCounter:number = 0;
+
+  btnCounter(){
+    this.counter++;
+  }
+
+  showHeader(){
+    this.hide = !this.hide; // switch, shorter than if statement
+
+  }
 }
